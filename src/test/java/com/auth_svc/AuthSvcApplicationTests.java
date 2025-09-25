d46788bab6770239daced41f6f4b9153de26bbe5
huypqse <huypqse@gmail.com>
2025-09-26 00:07:27 +0700

    feat: add project base for auth_svc

---
+++ src/test/java/com/auth_svc/AuthSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
