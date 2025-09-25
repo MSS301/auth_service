ce76454d7747c2196731c5a85efc8e35649fa113
huypqse <huypqse@gmail.com>
2025-09-26 00:46:05 +0700

    feat: add introspect resp

---
+++ src/main/java/com/auth_svc/auth/dto/response/IntrospectResponse.java
@@ -0,0 +1,13 @@
+package com.auth_svc.auth.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectResponse {
+    boolean valid;
+}
