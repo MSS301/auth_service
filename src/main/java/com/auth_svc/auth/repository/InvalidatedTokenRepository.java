5f6ca525690051b4c4c7d72a0014f60882956d22
huypqse <huypqse@gmail.com>
2025-09-26 00:54:01 +0700

    feat: add InvalidatedTokenRepository

---
+++ src/main/java/com/auth_svc/auth/repository/InvalidatedTokenRepository.java
@@ -0,0 +1,9 @@
+package com.auth_svc.auth.repository;
+
+import com.auth_svc.auth.entity.InvalidatedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {}
