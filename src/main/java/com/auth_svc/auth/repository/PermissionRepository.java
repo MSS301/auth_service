97942477a66d55cda5a414d092ef1e76d7f0fecf
huypqse <huypqse@gmail.com>
2025-09-26 00:55:19 +0700

    feat: add PermissionRepository

---
+++ src/main/java/com/auth_svc/auth/repository/PermissionRepository.java
@@ -0,0 +1,8 @@
+package com.auth_svc.auth.repository;
+
+import com.auth_svc.auth.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, String> {}
