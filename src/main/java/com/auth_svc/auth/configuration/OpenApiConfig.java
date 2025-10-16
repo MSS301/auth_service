a8802bdc8abf055184d2cf1cca942ae4b412af14
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-10-16 12:46:52 +0700

    feat: add swagger

---
+++ src/main/java/com/auth_svc/auth/configuration/OpenApiConfig.java
@@ -0,0 +1,47 @@
+package com.auth_svc.auth.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI authServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auth Service API")
+                        .version("1.0.0")
+                        .description("Authentication and Authorization Service API Documentation")
+                        .contact(new Contact()
+                                .name("Your Team")
+                                .email("team@example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("API Gateway"),
+                        new Server()
+                                .url("http://localhost:8081/auth")
+                                .description("Auth Service Direct")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
+}
