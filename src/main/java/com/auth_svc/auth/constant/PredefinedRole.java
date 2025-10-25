338b3ddfde72e2155d3eb19293958ed1b5f03c37
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-10-25 14:50:37 +0700

    feat: add role teacher

--- src/main/java/com/auth_svc/auth/constant/PredefinedRole.java
+++ src/main/java/com/auth_svc/auth/constant/PredefinedRole.java
@@ -3,6 +3,7 @@ package com.auth_svc.auth.constant;
 public class PredefinedRole {
     public static final String USER_ROLE = "USER";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String TEACHER_ROLE = "TEACHER";
 
     private PredefinedRole() {}
 }
