048ceb77cd0fa705b6ce1c671e925d3a88278c6c
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-10-25 14:51:00 +0700

    feat: add email send feature

--- src/main/java/com/auth_svc/auth/dto/request/AuthenticationRequest.java
+++ src/main/java/com/auth_svc/auth/dto/request/AuthenticationRequest.java
@@ -1,5 +1,8 @@
 package com.auth_svc.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -9,6 +12,10 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthenticationRequest {
-    String username;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    String email;
+
+    @NotBlank(message = "Password is required")
     String password;
 }
